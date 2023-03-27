cffb6275e8028069e3833d6d07cddd6c46930df4
AlexasandrYakovlef <aleksandar.iack0vlev@mail.ru>
2023-03-27 22:49:37 +0300

    JSX+Компоненты

--- www/src/App.js
+++ www/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Header from './components/Header'
+import Image from './components/Image'
+import logo from "./img/logo.jpg"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  helpText = "Help Text!"
+  render() {
+    return (<div className="name">
+        <Header title="Кепка" />
+        <h1>{this.helpText}</h1>
+        <input placeholder={this.helpText}
+            onClick={this.inputClick} onMouseEnter={this.mouceOver}/>
+        <p>{this.helpText === "Help Text!" ? "Yes" : "No"}</p>
+        <Image image={logo}/>
+        <img src={logo} alt="" />
+      </div>)
+  }
+
+  inputClick() {console.log("Click")}
+  mouceOver() {console.log("Mouse Over")}
 }
 
-export default App;
+export default App
