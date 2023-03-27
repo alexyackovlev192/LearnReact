cc2e91d18f856060716728a855f29fe8938c9245
AlexasandrYakovlef <aleksandar.iack0vlev@mail.ru>
2023-03-27 23:31:34 +0300

    Состояния

--- www/src/App.js
+++ www/src/App.js
@@ -4,20 +4,40 @@ import Image from './components/Image'
 import logo from "./img/logo.jpg"
 
 class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            helpText: "HelpText",
+            userData: ""
+        }
+
+        this.inputClick = this.inputClick.bind(this)
+    }
+
+    componentDidUpdate(prevProp){
+        if(this.state.helpText !== "Help")
+            console.log("Some")
+    }
+
   helpText = "Help Text!"
   render() {
     return (<div className="name">
         <Header title="Кепка" />
-        <h1>{this.helpText}</h1>
+        <h1>{this.state.helpText}</h1>
+        <h2>{this.state.userData}</h2>
         <input placeholder={this.helpText}
+            onChange={event => this.setState({userData: event.target.value})}
             onClick={this.inputClick} onMouseEnter={this.mouceOver}/>
-        <p>{this.helpText === "Help Text!" ? "Yes" : "No"}</p>
+        <p>{this.state.helpText === "Help Text!" ? "Yes" : "No"}</p>
         <Image image={logo}/>
-        <img src={logo} alt="" />
+        {/* <img src={logo} alt="" /> */}
       </div>)
   }
 
-  inputClick() {console.log("Click")}
+  inputClick() {
+    this.setState({helpText: "Changed"})
+    console.log("Click")
+}
   mouceOver() {console.log("Mouse Over")}
 }
 
--- www/src/components/Header.js
+++ www/src/components/Header.js
@@ -1,9 +1,14 @@
 import React from "react"
+import Button from "./Button"
 
 class Header extends React.Component {
   render() {
-    return (
-      <header className="header">{this.props.title}</header>
+    return ( 
+      <header className="header">
+        {this.props.title}
+        <br></br>
+        <Button text="But"/>
+      </header>
     )
   }
 }
